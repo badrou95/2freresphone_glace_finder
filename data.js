b790c62cd0c71bb0ca463faa1050c9ccd62f5b43
BaDr eddine Tigrine <badroumc@gmail.com>
2025-07-10 22:53:05 +0100

    Add files via upload

--- data.js
+++ data.js
@@ -1,23 +1,14 @@
 
-const phones = [
-  {
-    model: "Samsung A71",
-    compatibles: ["Samsung A72", "Samsung M51", "Samsung S20 FE", "Samsung A70", "Samsung A80"],
-    image: "images/samsung_a71.jpg"
-  },
-  {
-    model: "Samsung A72",
-    compatibles: ["Samsung A71", "Samsung M51", "Samsung A73"],
-    image: "images/samsung_a72.jpg"
-  },
-  {
-    model: "iPhone 11",
-    compatibles: ["iPhone XR", "iPhone 12"],
-    image: "images/iphone_11.jpg"
-  },
-  {
-    model: "iPhone XR",
-    compatibles: ["iPhone 11"],
-    image: "images/iphone_xr.jpg"
-  }
-];
+    const phoneData = [
+        {
+            model: "Samsung A71",
+            compatible_with: ["Samsung M51", "Samsung S20 FE", "Oppo A91", "Realme 7 Pro"],
+            image: "a71.jpg"
+        },
+        {
+            model: "iPhone 13",
+            compatible_with: ["iPhone 14", "iPhone 13 Pro"],
+            image: "iphone13.jpg"
+        }
+    ];
+    
---
+++ service-worker.js
@@ -0,0 +1,1 @@
+// Service Worker pour PWA
